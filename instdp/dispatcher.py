8a2dbf33aa1f9a1a03cb8927cf42865caaff5022
Teri <75248049+Teri-anric@users.noreply.github.com>
2023-07-10 21:25:03 +0300

    update dispatcher.py

--- instdp/dispatcher.py
+++ instdp/dispatcher.py
@@ -1,6 +1,7 @@
 from instagrapi import Client
 from .types.handler import Handler
 
+
 class InstDispatcher:
     def __init__(self, client: Client):
         self._cl = client
@@ -11,5 +12,7 @@ class InstDispatcher:
             self._message_handlers.append(Handler(func))
 
 
-    def polling(self, interval: int = 5, auto_seen: bool = True):
-        self._cl.direct_threads()
+    def polling_direct(self, interval: int = 10, selected_filter = "unread", auto_seen: bool = True):
+        threads = self._cl.direct_threads(selected_filter = selected_filter, thread_message_limit = 20)
+        for thread in threads:
+            pass

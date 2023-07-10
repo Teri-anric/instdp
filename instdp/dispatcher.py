9d0ec619c7fdd5a1464b022b893155eda8ff73d7
Teri <75248049+Teri-anric@users.noreply.github.com>
2023-07-10 21:52:28 +0300

    update dispatcher.py

--- instdp/dispatcher.py
+++ instdp/dispatcher.py
@@ -1,18 +1,20 @@
 from instagrapi import Client
 from .types.handler import Handler
 
+from instagrapi.extractors import (
+    extract_direct_response
+)
+from instagrapi.types import (
+    DirectMessage,
+    DirectResponse,
+    DirectShortThread,
+    DirectThread,
+    Media,
+)
+
 
 class InstDispatcher:
+    
     def __init__(self, client: Client):
         self._cl = client
-        self._message_handlers = []
-
-    def message(self):
-        def decorator(func):
-            self._message_handlers.append(Handler(func))
-
-
-    def polling_direct(self, interval: int = 10, selected_filter = "unread", auto_seen: bool = True):
-        threads = self._cl.direct_threads(selected_filter = selected_filter, thread_message_limit = 20)
-        for thread in threads:
-            pass
+        super().__init__()
